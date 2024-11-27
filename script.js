2e2564fbc1eacf211555bace4d7b585fe0b96770
YasminaN1 <526ynigmanova@frhsd.com>
2024-11-27 09:38:21 -0500

    added letter text

---
+++ script.js
@@ -0,0 +1,7 @@
+Dear ${username},
+
+I hope this letter finds you feeling ${adjective1} and ready for an adventure! The Pogues and I have discovered an old ${noun1} that could lead us straight to the legendary gold of the Outer Banks.
+
+We’ll be meeting at ${place} at ${time} to begin our journey. Make sure to pack a ${adjective1} ${noun1} and don’t forget your ${pluralNoun1}—trust me, we’ll need them.
+
+If we run into trouble, just yell "${exclamation}" and we’ll stick to the plan. I can’t wait to see you there—let’s make history, Pogues-style!
