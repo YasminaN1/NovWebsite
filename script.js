c27cb0536c7188bbf03b2e3a831e56a6c91ffc0d
YasminaN1 <526ynigmanova@frhsd.com>
2024-11-30 20:16:58 +0000

    changed up the prompts

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-Dear ${username},
+let userName = prompt('What is your name?')
+let adjective1 = prompt('Pick a positive emotion/feeling!')
+let place = prompt('Where would you like to begin your journey?')
+let time = prompt('What time do you think is best?')
+let adjective2 = prompt('Enter an adjective to describe the item! e.g., sturdy, lightweight')
+let noun1 = prompt('Enter a noun to specify the item! e.g., backpack, compass')
+let pluralNoun1 = prompt('Enter a plural noun for something essential to bring! e.g., flashlights, snacks, ropes')
+let exclamation = prompt('make up a code word if something dangerous or exciting happens!')
 
-I hope this letter finds you feeling ${adjective1} and ready for an adventure! The Pogues and I have discovered an old ${noun1} that could lead us straight to the legendary gold of the Outer Banks.
+Dear ${userName},
 
-We’ll be meeting at ${place} at ${time} to begin our journey. Make sure to pack a ${adjective1} ${noun1} and don’t forget your ${pluralNoun1}—trust me, we’ll need them.
+I hope this letter finds you feeling ${adjective1} and ready for an adventure! The Pogues and I have discovered an old map that could lead us straight to the legendary gold of the Outer Banks.
+
+We’ll be meeting at ${place} at ${time} to begin our journey. Make sure to pack a ${adjective2} ${noun1} and don’t forget your ${pluralNoun1}—trust me, we’ll need them.
 
 If we run into trouble, just yell "${exclamation}" and we’ll stick to the plan. I can’t wait to see you there—let’s make history, Pogues-style!
